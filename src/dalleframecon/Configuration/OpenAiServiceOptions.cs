4b2fb2b8732b19bc74d258f9cde8453c0902134d
Jim Bennett <jim.bennett@microsoft.com>
2023-04-17 10:35:19 -0700

    Moving options to the right file

--- src/dalleframecon/Configuration/OpenAiServiceOptions.cs
+++ src/dalleframecon/Configuration/OpenAiServiceOptions.cs
@@ -1,62 +1,22 @@
 ﻿namespace dalleframecon.Configuration
 {
     /// <summary>
-    /// Configuration options class for interacting with Azure Cognitive Services.
+    /// Configuration options for interacting with OpenAI.
     /// </summary>
-    public class AzureCognitiveServicesOptions
+    internal class OpenAiServiceOptions
     {
         /// <summary>
-        /// Location/region (e.g. WestUS3)
-        /// </summary>
-        public string Region { get; set; }
-
-        /// <summary>
-        /// Access Key
+        /// API Key.
         /// </summary>
         public string Key { get; set; }
-
-        /// <summary>
-        /// Default language for speech recognition (speech-to-text).
-        /// </summary>
-        public string SpeechRecognitionLanguage { get; set; }
-
-        /// <summary>
-        /// Name of the voice to use for speaking (text-to-speech).
-        /// </summary>
-        /// <remarks>
-        /// https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support?tabs=stt-tts#text-to-speech
-        /// </remarks>
-        public string SpeechSynthesisVoiceName { get; set; }
-
-        /// <summary>
-        /// Endpoint of multi-service resource.
-        /// </summary>
-        public string Endpoint { get; set; }
-
-        /// <summary>
-        /// Filename of the wake phrase model to use.
-        /// </summary>
-        public string WakePhraseModel { get; set; }
 
         /// <summary>
         /// Validate options, throw an exception is any are invalid.
         /// </summary>
         public void Validate()
         {
-            if (string.IsNullOrWhiteSpace(Region))
-                throw new ArgumentException("Argument is invalid.", nameof(Region));
-
             if (string.IsNullOrWhiteSpace(Key))
                 throw new ArgumentException("Argument is invalid.", nameof(Key));
-
-            if (string.IsNullOrWhiteSpace(SpeechRecognitionLanguage))
-                throw new ArgumentException("Argument is invalid.", nameof(SpeechRecognitionLanguage));
-
-            if (string.IsNullOrWhiteSpace(SpeechSynthesisVoiceName))
-                throw new ArgumentException("Argument is invalid.", nameof(SpeechSynthesisVoiceName));
-
-            if (string.IsNullOrWhiteSpace(Endpoint))
-                throw new ArgumentException("Argument is invalid.", nameof(Endpoint));
         }
     }
 }
